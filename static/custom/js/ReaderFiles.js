b70a084e0b361f7fbf51be9410f23cb5406d54d4
Polar <jvasconsuelom@gmail.com>
2017-06-01 04:47:41 -0500

    changes

--- static/custom/js/ReaderFiles.js
+++ static/custom/js/ReaderFiles.js
@@ -64,7 +64,7 @@ function readBlob(id_input, id_content, id_error ) {
                             {
                                 field: /^(\s|\s\s+|)(\w+)(\s|\s\s+|)/gi.exec(row)[0].trim(),
                                 type: /models.(\w+)/gi.exec(row)[1].trim(),
-                                kwargs: row.match(/\(.*(.*)\)$/gi)[0].split(',')
+                                kwargs: row.match(/\(.*(.*)\)$/gi)[0].replace(')', '').replace('(', '').replace(' ', '').split(',')
                             }
                         )
                     }
@@ -132,42 +132,134 @@ function readBlob(id_input, id_content, id_error ) {
 
 }
 
-function procesar(){
+function procesarPaso1(){
+    var acordion = "" +
+        "<div class='panel-group' data-collapse-color='green' id='accordionRed' role='tablist' aria-multiselectable='true' style='margin-bottom: auto;'>";
+
     $("input[type=checkbox]:checked").each(function(){
         var checkbox_id = $(this).attr("id");
         var item = file_content[checkbox_id];
-        // headers
-        $('.tab-nav li').last().after("<li class='waves-effect' role='presentation'><a href='#" + item.model + "' aria-controls='profile9' role='tab' data-toggle='tab'>" + item.model + "</a></li>");
 
-        var table = "<div class='table-responsive'>" +
+        // headers
+        // $('.tab-nav li').last().after("<li class='waves-effect' role='presentation'><a href='#" + item.model + "' aria-controls='profile9' role='tab' data-toggle='tab'>" + item.model + "</a></li>");
+
+        var table = "<div class='table-responsive '>" +
                             "<table class='table table-hover bluegray'>" +
-                                "<tbody>";
+                                "<tbody class='simple_with_drop'>";
         for(var i in item.fields){
             var field = item.fields[i];
-                table += "<tr>" +
-                            "<td style='padding: 1px;'>" +
-                                    "<div class='checkbox'>" +
-                                        "<label>" +
-                                            "<input id='"+i+"' type='checkbox' value='' checked>" +
-                                            "<i class='input-helper'></i>" +
-                                            field.field +
-                                        "</label>" +
-                                    "</div>" +
-                            "</td>" +
-                        "</tr>" ;
-        }
+            var required = 'checked';
+            var auto_now_add = false;
+            var max_length = '';
+            var min_length = '';
+            for(var key in field.kwargs){
+                var kwarg = field.kwargs[key];
+                kwarg = kwarg.trim();
+                kwarg = kwarg.split('=');
+                if (kwarg[0]=='null'){
+                   required = kwarg[1]=='True'? '': 'checked';
+                }
+                else if (kwarg[0]=='auto_now_add'){
+                   auto_now_add = kwarg[1]=='True';
+                }
+                else if (kwarg[0]=='max_length'){
+                   max_length = kwarg[1];
+                }
+                else if (kwarg[0]=='min_length'){
+                   min_length = kwarg[1];
+                }
+            }
 
+            if(!auto_now_add){
+
+                table +=
+                    "<tr class='icon-move'>" +
+                        "<td style='padding: 1px;'>" +
+                            "<div class='checkbox'>" +
+                                "<label>" +
+                                    "<input id='"+i+"' type='checkbox' value='' checked>" +
+                                    "<i class='input-helper'></i>" +
+                                    field.field + "  ( " + field.type +" )" +
+                                "</label>" +
+                            "</div>" +
+                        "</td>" +
+                        "<td style='padding: 1px;'>" +
+                            "<div class='checkbox'>" +
+                                "<label>" +
+                                    "<input id='"+i+"' type='checkbox' value='' "+ required +">" +
+                                    "<i class='input-helper'></i> Required" +
+                                "</label>" +
+                            "</div>" +
+                        "</td>" +
+                        "<td style='padding: 1px;'>" +
+                            "<div class='checkbox'>" +
+                                "<label>" +
+                                    "<input id='"+i+"' type='checkbox' value=''>" +
+                                    "<i class='input-helper'></i> Disabled" +
+                                "</label>" +
+                            "</div>" +
+                        "</td>"
+
+
+                if(field.type != "ForeignKey" && field.type != "BooleanField" && field.type != "DateTimeField"){
+                    table += "<td style='width: 12%'>" +
+                                "<input type='text' class='form-control' id='" + field.field +"-regex' value='" + max_length +"' style='width: 20%; display: inline-block;'> Max Length" +
+                            "</td>" +
+                            "<td style='width: 12%'>" +
+                                "<input type='text' class='form-control' id='" + field.field +"-regex' value='" + min_length +"' style='width: 20%; display: inline-block;'> Min Length" +
+                            "</td>" +
+                            "<td style='width: 30%'>" +
+                                "<input type='text' class='form-control' placeholder='Regexp'  id='" + field.field +"-regex' style='width: 80%; display: inline-block;'> Regexp" +
+                            "</td>"
+                }
+                else{
+                    table += "<td></td><td></td><td></td>"
+                }
+
+                table += "</tr>" ;
+
+            }
+
+        }
         table +=  "</tbody>" +
                    "</table>" +
                 "</div>";
 
-        // bodies
-        $(".tab-content div[role='tabpanel']").last().after("<div role='tabpanel' class='tab-pane animated fadeInRight' id='" + item.model + "'>" + table + "</div>");
-
-        $('#modalColor').modal('hide');
-    })
 
 
+        acordion +=
+            // "<div class='row'>" +
+            // "<div class='col-sm-12'>" +
+
+                    "<div class='panel panel-collapse'>" +
+                        "<div class='panel-heading' role='tab'>" +
+                            "<h4 class='panel-title'>" +
+                                "<a data-toggle='collapse' data-parent='#accordionRed' href='#" + item.model +"' aria-expanded='false'>" +
+                                    " " + item.model + " " +
+                                "</a>" +
+                            "</h4>" +
+                        "</div>" +
+                        "<div id='" + item.model +"' class='collapse' role='tabpanel'>" +
+                            // "<div class='panel-body'>" +
+                                table +
+                            // "</div>" +
+                        "</div>" +
+                    "</div>"
+            // "</div>" +
+        // "</div>"
+
+
+    });
+    acordion += "</div>";
+    // bodies
+    $("#paso2-acordion").html(acordion);
+    $('a[href=#paso2]').click();
+    $('#modalColor').modal('hide');
+
+}
+
+function continuarPaso2(){
+    $('a[href=#paso3]').click();
 }
 
 // evalua si un HTML Element, una lista o un string no es valido
